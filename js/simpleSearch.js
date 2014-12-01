9f5dc4901778e6e52390798c0173933047a02139
mciustea <mciustea@uw.edu>
2014-12-01 11:52:32 -0800

    Reduced text displayed to 50 words

--- js/simpleSearch.js
+++ js/simpleSearch.js
@@ -51,7 +51,7 @@ function renderResults(docs, $container, $template){
             .find( "h3" )
             .append( doc.title );
         result.find( ".url" ).append( doc.url );
-        result.find( ".content" ).append( maxWords(doc.content, 100) );
+        result.find( ".content" ).append( maxWords(doc.content, 50) );
         result.removeClass( "template" );
         $container.append(result);
     });
